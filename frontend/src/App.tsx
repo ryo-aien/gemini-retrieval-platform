f4e2273241fb0df0e9e2956fca62b8e86d38a7e7
ryoshimu <snd.hp0015@gmail.com>
2025-12-25 16:35:28 +0900

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
 import SourcePanel from './components/SourcePanel';
 import ChatPanel from './components/ChatPanel';
